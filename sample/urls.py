f8216e585a1f7d16dc5866cb384d6b668a5ec2e8
makhija0211 <mahimamakhija0211@gmail.com>
2018-06-05 11:21:43 +0530

    commit check

--- sample/urls.py
+++ sample/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('expense_tracker_app',include('expense_tracker_app.urls')),
     path('admin/', admin.site.urls),
 ]
